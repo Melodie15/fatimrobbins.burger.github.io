9bb3f70ec1645ff7ab23ebeac6167efe7058e0b6
frobbins <robbinsfatim@gmail.com>
2020-09-20 00:35:15 -0600

    created models folder, burger.js file and imported orm.js into burger.js

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+// These are the methods you will need to use in order to retrieve and store
+// selectAll()
+// insertOne()
+// updateOne()
+
+
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
